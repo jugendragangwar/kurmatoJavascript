5d0b0551e6486aae06e894cdfc19ccfa7cbdf178
jugendragangwar <jugendragangwar8791@gmail.com>
2024-07-08 16:09:00 +0530

    Commit

---
+++ String/stringPrograms/containsASubstring.js
@@ -0,0 +1,4 @@
+const string="This code for check substring in string";
+const substring=prompt("Enter substring");
+
+console.log(string.includes(substring));
---
+++ String/stringPrograms/reverseString.js
@@ -0,0 +1,3 @@
+let  string=prompt("ENter a String");
+let reversed = string.split('').reverse().join('');
+document.write("The string" + " " + string + " " + "after reversed" + " = " + reversed);
