10fc4b3d9bd04b7ba1e7e5aebe50a5f297e28ab0
jugendragangwar <jugendragangwar8791@gmail.com>
2024-07-08 16:09:51 +0530

    Commit

--- String/stringPrograms/reverseString.js
+++ String/stringPrograms/reverseString.js
@@ -1,3 +1,3 @@
 let  string=prompt("ENter a String");
-let reversed = string.split('').reverse().join('');
+let reversed = string.split(' ').reverse().join(' ');
 document.write("The string" + " " + string + " " + "after reversed" + " = " + reversed);
