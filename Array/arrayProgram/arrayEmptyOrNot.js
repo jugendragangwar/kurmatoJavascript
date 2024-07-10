0f91751de5ad75663c8fe1dfbaa057a9be05d72d
jugendragangwar <jugendragangwar8791@gmail.com>
2024-07-10 15:00:28 +0530

    Add some program of Array

---
+++ Array/arrayProgram/arrayEmptyOrNot.js
@@ -0,0 +1,11 @@
+const array=[55,45];
+function checkArrayIsEmptyOrNot(array){
+    if((array.length)===0){
+     return "Array is Empty";
+    }
+    else {
+        return "Array is not Empty";
+    }
+}
+console.log(checkArrayIsEmptyOrNot(array))
+
