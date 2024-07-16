f98ffaef2630065e350fab059feceb0e38adadc8
jugendragangwar <jugendragangwar8791@gmail.com>
2024-07-16 15:24:00 +0530

    change

--- run.js
+++ run.js
@@ -1,14 +1,54 @@
-function one(){
+/*
+function one() {
     console.log("One");
     two()
 }
-function two(){
+function two() {
     console.log("Two");
     three()
 }
-function three(){
+function three() {
     console.log("Three");
 }
 one()
 two()
 three()
+
+function myFunction(num1, num2) {
+    console.log("Hello");
+    return num1 + num2
+}
+const result = myFunction(43, 54);
+console.log(result);
+
+function loginuUerMassage(user) {
+    if (user === undefined) {
+        console.log("Enter user Name");
+        return
+    }
+    return `${user} just logged in `
+}
+console.log(loginuUerMassage());
+
+function calculateCartPrice(val1, val2, ...num1) {
+    // (...num)
+    return num1
+}
+console.log(calculateCartPrice(200, 500, 300, 407, 767, 322));
+
+*/
+const user = {
+    username: "Hari",
+    price: 399
+}
+
+// function handleObject(anyobject) {
+//     console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+
+const newArray =[200,322,433,566]
+
+function returnSectionValue(getArray){
+    return getArray[1]
+}
+console.log(returnSecondValue(mynew));
+
