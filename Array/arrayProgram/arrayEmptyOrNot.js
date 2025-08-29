ae4727cee46c320115a49d4e3b4e15c9f46dc618
jugendragangwar <jugendragangwar8791@gmail.com>
2025-08-29 14:55:55 +0530

    Solve a Array element frequency Question

--- Array/arrayProgram/arrayEmptyOrNot.js
+++ Array/arrayProgram/arrayEmptyOrNot.js
@@ -1,17 +1,13 @@
-// const array=[55,45];
-// function checkArrayIsEmptyOrNot(array){
-//     if((array.length)===0){
-//      return "Array is Empty";
-//     }
-//     else {
-//         return "Array is not Empty";
-//     }
-// }
-// console.log(checkArrayIsEmptyOrNot(array))
+const array=[55,45];
+function checkArrayIsEmptyOrNot(array){
+    if((array.length)===0){
+     return "Array is Empty";
+    }
+    else {
+        return "Array is not Empty";
+    }
+}
+console.log(checkArrayIsEmptyOrNot(array))
 
 
 
-
-import { data } from "../../data";
-
-console.log(data[1])
